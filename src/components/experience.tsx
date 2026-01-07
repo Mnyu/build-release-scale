41046469339ff18f9da68b0cffa60c93f84fce28
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2026-01-08 00:17:05 +0530

    experience section : change secondary color to foreground color and gap value

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -2,9 +2,9 @@ const Experience = () => {
   return (
     <section id="experience" className="min-h-screen snap-start max-w-screen-xl mx-auto px-4 relative">
       <h1 className="text-center px-4 pt-4 bg-background relative z-10">Experience</h1>
-      <div className="flex items-center gap-20 mt-10 relative">
+      <div className="flex items-center gap-50 mt-10 relative">
         <div className="h-[2px] bg-primary absolute top-1/2 left-1/4 right-1/2"></div>
-        <div className="w-4 h-4 rounded-full border-2 border-secondary absolute left-1/2 -translate-x-1/2 bg-background z-10 hover:scale-110 ease-in-out duration-100"></div>
+        <div className="w-4 h-4 rounded-full border-2 border-foreground absolute left-1/2 -translate-x-1/2 bg-background z-10 hover:scale-110 ease-in-out duration-100"></div>
         <div className="h-full w-full p-4 relative bg-background border rounded-xl">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid dignissimos explicabo neque atque harum,
           veniam tempore repellendus ducimus voluptatibus voluptatum!
@@ -14,9 +14,9 @@ const Experience = () => {
           veniam tempore repellendus ducimus voluptatibus voluptatum!
         </div>
       </div>
-      <div className="flex items-center gap-20 mt-10 relative">
+      <div className="flex items-center gap-50 mt-10 relative">
         <div className="h-[2px] bg-primary absolute top-1/2 left-1/4 right-1/2"></div>
-        <div className="w-4 h-4 rounded-full border-2 border-secondary absolute left-1/2 -translate-x-1/2 bg-background z-10 hover:scale-110 ease-in-out duration-100"></div>
+        <div className="w-4 h-4 rounded-full border-2 border-foreground absolute left-1/2 -translate-x-1/2 bg-background z-10 hover:scale-110 ease-in-out duration-100"></div>
         <div className="h-full w-full p-4 relative bg-background border rounded-xl">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid dignissimos explicabo neque atque harum,
           veniam tempore repellendus ducimus voluptatibus voluptatum!
@@ -26,9 +26,9 @@ const Experience = () => {
           veniam tempore repellendus ducimus voluptatibus voluptatum!
         </div>
       </div>
-      <div className="flex items-center gap-20 mt-10 relative">
+      <div className="flex items-center gap-50 mt-10 relative">
         <div className="h-[2px] bg-primary absolute top-1/2 left-1/4 right-1/2"></div>
-        <div className="w-4 h-4 rounded-full border-2 border-secondary absolute left-1/2 -translate-x-1/2 bg-background z-10 hover:scale-110 ease-in-out duration-100"></div>
+        <div className="w-4 h-4 rounded-full border-2 border-foreground absolute left-1/2 -translate-x-1/2 bg-background z-10 hover:scale-110 ease-in-out duration-100"></div>
         <div className="h-full w-full p-4 relative bg-background border rounded-xl">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid dignissimos explicabo neque atque harum,
           veniam tempore repellendus ducimus voluptatibus voluptatum!
@@ -38,9 +38,9 @@ const Experience = () => {
           veniam tempore repellendus ducimus voluptatibus voluptatum!
         </div>
       </div>
-      <div className="flex items-center gap-20 mt-10 relative">
+      <div className="flex items-center gap-50 mt-10 relative">
         <div className="h-[2px] bg-primary absolute top-1/2 left-1/4 right-1/2"></div>
-        <div className="w-4 h-4 rounded-full border-2 border-secondary absolute left-1/2 -translate-x-1/2 bg-background z-10 hover:scale-110 ease-in-out duration-100"></div>
+        <div className="w-4 h-4 rounded-full border-2 border-foreground absolute left-1/2 -translate-x-1/2 bg-background z-10 hover:scale-110 ease-in-out duration-100"></div>
         <div className="h-full w-full p-4 relative bg-background border rounded-xl">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid dignissimos explicabo neque atque harum,
           veniam tempore repellendus ducimus voluptatibus voluptatum!
@@ -50,9 +50,9 @@ const Experience = () => {
           veniam tempore repellendus ducimus voluptatibus voluptatum!
         </div>
       </div>
-      <div className="flex items-center gap-20 mt-10 relative">
+      <div className="flex items-center gap-50 mt-10 relative">
         <div className="h-[2px] bg-primary absolute top-1/2 left-1/4 right-1/2"></div>
-        <div className="w-4 h-4 rounded-full border-2 border-secondary absolute left-1/2 -translate-x-1/2 bg-background z-10 hover:scale-110 ease-in-out duration-100"></div>
+        <div className="w-4 h-4 rounded-full border-2 border-foreground absolute left-1/2 -translate-x-1/2 bg-background z-10 hover:scale-110 ease-in-out duration-100"></div>
         <div className="h-full w-full p-4 relative bg-background border rounded-xl">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid dignissimos explicabo neque atque harum,
           veniam tempore repellendus ducimus voluptatibus voluptatum!
