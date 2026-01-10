f296a0c3e9fd22626f001cfbf15abfddceabf992
Abhimanyu <mail2abhimanyu.gupta@gmail.com>
2026-01-10 18:39:08 +0530

    case-studies : nextra docs setup with 2 sample pages

---
+++ src/app/case-studies/databases/_meta.tsx
@@ -0,0 +1,9 @@
+export default {
+  index: {
+    title: "General",
+    display: "hidden",
+  },
+  oracle: {
+    title: "Oracle",
+  },
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import Projects from "@/components/projects";
 import Experience from "@/components/experience";
 import Home from "@/components/home";
 import Sidebar from "@/components/sidebar";
 import AiAgents from "@/components/ai-agents";
+import { Button } from "@/components/ui/button";
 
 const Page = () => {
   return (
@@ -13,6 +15,11 @@ const Page = () => {
       <AiAgents />
       <section id="case-studies" className="h-screen snap-start">
         <h1 className="text-center pt-2">Case Studies</h1>
+        <div className="mx-10 my-10">
+          <Link href="/case-studies">
+            <Button>My Case Studies</Button>
+          </Link>
+        </div>
       </section>
       <Projects />
     </main>
